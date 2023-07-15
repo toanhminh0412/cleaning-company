aae5b21f20a450371852e501e845f66cd148cc52
Archie To <toanhminh0412@gmail.com>
2023-07-14 22:41:17 -0700

    Initial commit

--- src/app/components/UpperNav.js
+++ src/app/components/UpperNav.js
@@ -1,19 +1,21 @@
+import Link from "next/link"
+
 export default function UpperNav() {
     return (
         <div className="fixed top-0 left-0 w-screen z-40">
             <div className="navbar bg-gradient-to-r from-red-500 to-orange-500 shadow-lg text-white">
                 <div className="navbar-start">
-                    <a className="btn btn-ghost normal-case text-xl">CleaningCompany</a>
+                    <Link href="#" className="btn btn-ghost normal-case text-xl">CleaningCompany</Link>
                 </div>
                 <div className="navbar-center hidden md:flex prose text-white text-xl font-light">
                     We clean and wash, and you relax.
                 </div>
                 <div className="navbar-end">
-                    <a className="btn btn-warning">Get a quote</a>
+                    <Link href="mailto:user@example.org" className="btn btn-warning">Get a quote</Link>
                 </div>
             </div>
             <div className="p-2 font-light bg-neutral-800 flex text-white">
-                <div><i className="fa-solid fa-envelope me-2 text-yellow-500"></i> user@example.org</div>
+                <div><i className="fa-solid fa-envelope me-2 text-yellow-500"></i> <Link href="mailto:user@example.org">user@example.org</Link></div>
                 <span className="mx-8"> | </span>
                 <div><i className="fa-solid fa-phone me-2 text-yellow-500"></i> 250-884-5157</div>
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="pt-32 pb-20">
-      <section className="flex flex-row flex-wrap md:flex-nowrap justify-center gap-x-4 w-11/12 mx-auto">
-        <div className="card w-11/12 md:w-1/3 bg-base-100 shadow-xl border border-slate-200 p-10">
-            <img src="/img/profile.jpeg" alt="Shoes" className="rounded-xl w-full max-w-sm xl:w-10/12 aspect-square mx-auto" />
+      <section className="flex flex-row flex-wrap lg:flex-nowrap justify-center gap-x-4 w-11/12 mx-auto">
+        <div className="card w-11/12 lg:w-1/3 bg-base-100 shadow-xl border border-slate-200 p-10">
+            <div className="relative rounded-xl w-full max-w-sm xl:w-10/12 aspect-square mx-auto">
+              <Image src="/img/profile.jpeg" alt="Shoes" fill />
+            </div>
           <div className="card-body items-center text-center prose">
             <h2 className="card-title">Maribel Bautista Nuval</h2>
             <p className="mt-[-30px] font-light text-slate-500 text-xl">Cleaning professional</p>
             <h3 className="mt-[-10px] font-semibold text-red-500">30 years of experience!</h3>
           </div>
         </div>
-        <div className="card w-11/12 mt-6 md:mt-0 md:w-2/3 bg-base-100 shadow-xl border border-slate-200 p-10">
+        <div className="card w-11/12 mt-6 lg:mt-0 lg:w-2/3 bg-base-100 shadow-xl border border-slate-200 p-10">
           <div className="prose">
             <article>
               <h1 className="underline underline-offset-[10px] decoration-orange-500">About me</h1>
@@ -20,7 +23,7 @@ export default function Home() {
               <h2>We provide quality services only!</h2>
             </article>
             <div>Feel free to contact me at:</div>
-            <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-2 mt-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-2 gap-2 mt-4">
               <div><i className="fa-solid fa-phone text-yellow-500 text-xl me-2"></i> 250-884-5157</div>
               <div><i className="fa-solid fa-envelope text-red-500 text-xl me-2"></i> <Link href="mailto:user@example.org">user@example.org</Link></div>
               <div><i className="fa-brands fa-facebook text-blue-500 text-xl me-2"></i> <Link href="https://www.facebook.com/mabel.n.sanchez1" target="_blank">Maribel Bautista Nuval</Link></div>
@@ -55,6 +58,78 @@ export default function Home() {
               <p>A clean place is essential for health, especially for seniors. Whether if it is an apartment, condo or a house, book a cleaning session for you or your parents right now! I will make sure they have the best environment to live in.</p>
             </div>
           </div>
+      </section>
+      
+      <section className="w-11/12 mx-auto mt-10">
+        <div className="prose">
+            <h1 className="underline underline-offset-[10px] decoration-orange-500">My past works</h1>
+        </div>
+        <div className="carousel rounded-box mt-8 border border-slate-500 shadow-xl w-full">
+          <div className="carousel-item border-r border-slate-500 relative w-80 aspect-video">
+            <Image src="/img/house1.jpg" alt="House pic" fill/>
+          </div> 
+          <div className="carousel-item border-r border-slate-500 relative w-80 aspect-video">
+            <Image src="/img/house2.jpg" alt="House pic" fill/>
+          </div> 
+          <div className="carousel-item border-r border-slate-500 relative w-80 aspect-video">
+            <Image src="/img/house3.jpg" alt="House pic" fill/>
+          </div> 
+          <div className="carousel-item border-r border-slate-500 relative w-80 aspect-video">
+            <Image src="/img/house4.jpg" alt="House pic" fill/>
+          </div> 
+          <div className="carousel-item border-r border-slate-500 relative w-80 aspect-video">
+            <Image src="/img/house5.jpg" alt="House pic" fill/>
+          </div> 
+          <div className="carousel-item border-r border-slate-500 relative w-80 aspect-video">
+            <Image src="/img/house6.jpg" alt="House pic" fill/>
+          </div> 
+        </div>
+      </section>
+      <section className="mt-10 w-11/12 mx-auto">
+        <div className="prose">
+          <h1 className="underline underline-offset-[10px] decoration-orange-500">Reviews</h1>
+        </div>
+        <div className="flex flex-row flex-wrap mt-12 gap-8">
+          <div className="card w-96 bg-base-100 shadow-xl border border-slate-300">
+            <div className="card-body prose">
+              <h2 className="card-title">Lee Perez</h2>
+              <div className="rating rating-md">
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nulla, eleifend ac ante sit amet, rhoncus porta nisl. Proin ac purus nec nulla fermentum ullamcorper. Sed quis dapibus ante. Proin vitae ante aliquet, aliquet nibh eu, semper mi. Integer eros risus, egestas non rhoncus non, feugiat ac odio.</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl border border-slate-300">
+            <div className="card-body prose">
+              <h2 className="card-title">Lee Perez</h2>
+              <div className="rating rating-md">
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nulla, eleifend ac ante sit amet, rhoncus porta nisl. Proin ac purus nec nulla fermentum ullamcorper. Sed quis dapibus ante. Proin vitae ante aliquet, aliquet nibh eu, semper mi. Integer eros risus, egestas non rhoncus non, feugiat ac odio.</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl border border-slate-300">
+            <div className="card-body prose">
+              <h2 className="card-title">Lee Perez</h2>
+              <div className="rating rating-md">
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nulla, eleifend ac ante sit amet, rhoncus porta nisl. Proin ac purus nec nulla fermentum ullamcorper. Sed quis dapibus ante. Proin vitae ante aliquet, aliquet nibh eu, semper mi. Integer eros risus, egestas non rhoncus non, feugiat ac odio.</p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   )
